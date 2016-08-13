bd5d4f9bddc0a7f2fdfdbf646bd194ea96577f2c
raggesilver <pvaqueiroz@gmail.com>
2016-08-13 13:23:24 -0300

    Progress circle and bugfixes

--- app/src/main/java/com/example/ctpsarah/feicon/MainActivity.java
+++ app/src/main/java/com/example/ctpsarah/feicon/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ctpsarah.feicon;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     int lastClicked;
 
+    boolean isSubmitting;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,16 +39,36 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        /*final ProgressDialog dialog=new ProgressDialog(MainActivity.this.getApplicationContext());
+        dialog.setMessage("message");
+        dialog.setCancelable(false);
+        dialog.setInverseBackgroundForced(false);
+        dialog.show();*/
+
+        findViewById(R.id.linearLayout1).setVisibility(View.INVISIBLE);
+
         WebViewClient mWebViewClient = new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 //test("App", "Gostou do app?", view);
+
+                if(isSubmitting){
+                    moveOn();
+                    return;
+                }
+
                 if(view.getUrl().toLowerCase().endsWith("quiz")){
-                    report();
                     startQuiz();
+                    findViewById(R.id.progressBar).setVisibility(View.GONE);
+                    findViewById(R.id.linearLayout1).setVisibility(View.VISIBLE);
+                    //dialog.hide();
                 } else if(view.getUrl().toLowerCase().endsWith("app")){
+                    //dialog.hide();
+                    findViewById(R.id.progressBar).setVisibility(View.GONE);
                     moveOn();
                 } else {
+                    //dialog.hide();
+                    findViewById(R.id.progressBar).setVisibility(View.GONE);
                     moveOn();
                 }
             }
@@ -56,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
         WebView _web = (WebView) findViewById(R.id.webView);
         _web.setWebViewClient(mWebViewClient);
-        _web.loadUrl("http://172.16.21.203/app_server/index.php?key=" + appKey + "&id=" + report());
+
+        _web.loadUrl("http://192.168.1.10/app_server/index.php?key=" + appKey + "&id=" + report());
 
         Button btn = (Button) findViewById(R.id.yesBtn);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 } else if ( currentStep > 3) { // Special number for after thank page
                     report();
                 }
-                showToast(""+currentStep);
+                // showToast(""+currentStep);
                 lastClicked = 1;
                 advance();
             }
@@ -165,9 +188,9 @@ public class MainActivity extends AppCompatActivity {
         check2.setVisibility(View.VISIBLE);
         check3.setVisibility(View.VISIBLE);
 
-        check1.setText("Alternativa 1");
-        check2.setText("Alternativa 2");
-        check3.setText("Alternativa 3");
+        check1.setText("A) A Epigenética é o estudo das reações que envolvem os gens e que são herdadas por transmissão genética de pai para filho. ");
+        check2.setText("B) A Epigenética é o estudo das reações que podem ativar ou desativar gens, por influência de fatores ambientais.");
+        check3.setText("C) A Epigenética é o estudo das reações que modificam os gens permanentemente em função de alterações genéticas efetuadas por meio de operações cirúrgicas.");
 
         TextView txt = (TextView) findViewById(R.id.label);
         txt.setText("Marque a opção que corresponde a Epigenética.");
@@ -211,6 +234,9 @@ public class MainActivity extends AppCompatActivity {
             check1.setText("Aluno Médio");
             check2.setText("Aluno Superior");
             check3.setText("Servidor");
+
+            noBtn.setVisibility(View.INVISIBLE);
+            yesBtn.setText("próximo");
         } else {
             check1.setVisibility(View.INVISIBLE);
             check2.setVisibility(View.INVISIBLE);
@@ -238,29 +264,34 @@ public class MainActivity extends AppCompatActivity {
 
         Button noBtn = (Button) findViewById(R.id.noBtn);
         noBtn.setVisibility(View.INVISIBLE);
+
+        showToast(""+currentStep);
     }
 
     public void advance() {
         Button btn = (Button) findViewById(R.id.yesBtn);
 
         switch (currentStep) {
-            case 0: btn.setBackgroundColor(Color.BLUE); // receive first answer (knows what epigenetics is)
+            case 0: // btn.setBackgroundColor(Color.BLUE); // receive first answer (knows what epigenetics is)
                     quizSecond();
                     currentStep++;
                     knowsEpigenetics = 0;
                     break;
-            case 1: btn.setBackgroundColor(Color.GREEN);
+            case 1: // btn.setBackgroundColor(Color.GREEN);
                     quizThird();
                     currentStep++;
                     break;
-            case 2: btn.setBackgroundColor(Color.RED);
+            case 2: // btn.setBackgroundColor(Color.RED);
+                    questionAnsw = checkedCheckbox();
                     quizLast(false);
                     currentStep++;
                     break;
-            case 3: btn.setBackgroundColor(Color.MAGENTA);
+            case 3: // btn.setBackgroundColor(Color.MAGENTA);
                     quizLast(true);
                     isIFTM = 1;
                     currentStep++;
+                    break;
+            case 5: submit();
                     break;
             default:if(isIFTM == 1){
                         CheckBox check1 = (CheckBox) findViewById(R.id.questionBtn1);
@@ -276,6 +307,7 @@ public class MainActivity extends AppCompatActivity {
                             type = 3; // Teacher/works at IFTM
                         }
                     }
+                    currentStep++;
                     thankPage();
         }
     }
@@ -316,6 +348,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public int checkedCheckbox() {
+
+        if(!validateCheckboxes()){
+            return 0;
+        }
+
+        CheckBox check1 = (CheckBox) findViewById(R.id.questionBtn1);
+        CheckBox check2 = (CheckBox) findViewById(R.id.questionBtn2);
+
+        if (check1.isChecked()) {
+            return 1;
+        } else if (check2.isChecked()) {
+            return 2;
+        } else return 3;
+    }
+
     public String report() {
         id = Secure.getString(MainActivity.this.getApplicationContext().getContentResolver(),Secure.ANDROID_ID);
         // showToast(id);
@@ -328,6 +376,25 @@ public class MainActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void submit() {
+
+        id = report();
+
+        WebView web = (WebView) findViewById(R.id.webView);
+
+        String _knows = "" + knowsEpigenetics;
+        String _questionAnsw = "" + questionAnsw;
+        String _isIFTM = "" + isIFTM;
+        String _type = "" + type;
+        String _id = id;
+
+        String _data = _knows + _questionAnsw + _isIFTM + _type + _id;
+
+        isSubmitting = true;
+        web.loadUrl("http://192.168.1.10/app_server/submit.php?key=" + "BR1QEJ5CWPNA" + "&answ=" + _data);
+
     }
 
 }
